e0c7eef59184b8a47101f9325a817a0ce2a809ee
glossner <john@glossner.org>
2023-11-14 06:37:32 -0500

    Factor  dictionaries to math_taxonmy.

--- compute_gephi.py
+++ compute_gephi.py
@@ -1,110 +1,20 @@
-topics_and_subtopics = {
-    "Arithmetic": [
-        "Basic operations",
-        "Fractions",
-        "Decimals",
-        "Percentages",
-        "Ratios and Proportions"
-    ],
-    "Algebra": [
-        "Algebraic Expressions",
-        "Linear Equations",
-        "Inequalities",
-        "Systems of Equations",
-        "Quadratic Equations",
-        "Polynomials",
-        "Functions"
-    ],
-    "Geometry": [
-        "Basic Geometric Shapes",
-        "Congruence and Similarity",
-        "Pythagorean Theorem",
-        "Circles",
-        "Area and Volume",
-        "Coordinate Geometry",
-        "Trigonometry"
-    ],
-    "Trigonometry": [
-        "Trigonometric Ratios",
-        "Graphs of Trigonometric Functions",
-        "Trigonometric Identities",
-        "Applications"
-    ],
-    "Statistics and Probability": [
-        "Descriptive Statistics",
-        "Probability Basics",
-        "Combinations and Permutations",
-        "Random Variables and Distributions",
-        "Inferential Statistics"
-    ],
-    "Pre-Calculus": [
-        "Advanced Algebra",
-        "Complex Numbers",
-        "Exponential and Logarithmic Functions",
-        "Advanced Trigonometry",
-        "Sequences and Series",
-        "Matrices"
-    ],
-    "Calculus": [
-        "Limits",
-        "Derivatives",
-        "Integration",
-        "Differential Equations",
-        "Multivariable Calculus"
-    ],
-    "Advanced Calculus (Honors)": [
-        "Series and Sequences",
-        "Vector Calculus",
-        "Multivariable Calculus",
-        "Differential Equations",
-        "Special Topics"
-    ],
-    "Discrete Mathematics (Honors)": [
-        "Logic and Proofs",
-        "Set Theory",
-        "Combinatorics",
-        "Graph Theory",
-        "Algorithms and Complexity"
-    ],
-    "Linear Algebra (Honors)": [
-        "Vector Spaces",
-        "Linear Transformations",
-        "Matrices",
-        "Systems of Linear Equations",
-        "Advanced Topics"
-    ],
-    "Advanced Statistics (Honors)": [
-        "Regression Analysis",
-        "ANOVA",
-        "Nonparametric Tests",
-        "Time Series Analysis",
-        "Bayesian Statistics"
-    ],
-    "Mathematical Proofs and Theory (Honors)": [
-        "Introduction to Proofs",
-        "Number Theory",
-        "Group Theory",
-        "Real Analysis",
-        "Topology"
-    ]
-}
 import math
+import math_taxonomy as mt
 
-import math
-
-def calculate_radius_for_spacing(num_points, individual_radius):
+def calculate_radius_for_spacing(num_points, individual_radius,separation_factor=1.5):
     """
     Calculate the radius of a circle needed to place a given number of points
     evenly spaced along its circumference, considering the size of each point.
     """
     # Each point occupies a space equal to its diameter along the circle's circumference
-    total_space_needed = num_points * 2 * individual_radius
+    total_space_needed = num_points * separation_factor * individual_radius
     # Calculate the radius needed to fit this total space on the circle's circumference
     return total_space_needed / (2 * math.pi)
-def generate_coordinates(topics_and_subtopics, individual_radius_main_topics, individual_radius_subtopics):
+
+def generate_coordinates(topics_and_subtopics, individual_radius_main_topics, individual_radius_subtopics,separation_main_topics,separation_sub_topics):
     num_main_topics = len(topics_and_subtopics.keys())
-    radius_main_topics = calculate_radius_for_spacing(num_main_topics, individual_radius_main_topics)
-    radius_subtopics = calculate_radius_for_spacing(sum([len(subtopics) for subtopics in topics_and_subtopics.values()]), individual_radius_subtopics)
+    radius_main_topics = calculate_radius_for_spacing(num_main_topics, individual_radius_main_topics,separation_main_topics)
+    radius_subtopics = calculate_radius_for_spacing(sum([len(subtopics) for subtopics in topics_and_subtopics.values()]), individual_radius_subtopics,separation_sub_topics)
 
     # Adjust the starting angle to place 'Arithmetic' just below the x-axis in the 3rd quadrant
     start_angle_main_topics = math.pi  # 180 degrees, pointing left
@@ -139,45 +49,61 @@ def round_coordinates(coordinates):
     """
     return [(round(x, 2), round(y, 2)) for x, y in coordinates]
 
-def generate_gephi_gdf(topics_and_subtopics, main_topic_coords, subtopic_coords, individual_radius_main_topics, individual_radius_subtopics):
-    gdf_content = "nodedef>name VARCHAR,label VARCHAR,width DOUBLE,x DOUBLE,y DOUBLE\n"
+def generate_gephi_gdf(topics_and_subtopics, main_topic_coords, subtopic_coords, individual_radius_main_topics, individual_radius_subtopics, topic_colors):
+    """
+    Generate a Gephi GDF file content with nodes and directed edges, including RGB colors for each topic.
+    Each topic node is connected to its subsequent topic and its subtopics. The color of each main topic is copied to its subtopics.
 
-    # Main topics
-    for topic, coords in zip(topics_and_subtopics.keys(), main_topic_coords):
-        x, y = coords
-        gdf_content += f"{topic},{topic},{individual_radius_main_topics},{x},{y}\n"
+    :param topics_and_subtopics: Dictionary with main topics as keys and lists of subtopics as values.
+    :param main_topic_coords: Coordinates of main topics.
+    :param subtopic_coords: Coordinates of subtopics.
+    :param individual_radius_main_topics: Radius of each main topic.
+    :param individual_radius_subtopics: Radius of each subtopic.
+    :param topic_colors: Dictionary with main topics as keys and their RGB color strings as values.
+    :return: String representing the content of a GDF file.
+    """
+    gdf_content = "nodedef>name VARCHAR,label VARCHAR,width DOUBLE,x DOUBLE,y DOUBLE,color VARCHAR\n"
 
-    # Subtopics
-    subtopic_coord_index = 0
-    for topic, subtopics in topics_and_subtopics.items():
+    # Add nodes for main topics with coordinates, width, and color
+    for i, (topic, subtopics) in enumerate(topics_and_subtopics.items()):
+        x, y = main_topic_coords[i]
+        color = f'"{topic_colors.get(topic, "255,255,255")}"'  # Enclose color in quotes
+        gdf_content += f"{topic},{topic},{individual_radius_main_topics},{x},{y},{color}\n"
+
+        # Add nodes for each subtopic with their coordinates and parent topic's color
         for subtopic in subtopics:
-            sub_x, sub_y = subtopic_coords[subtopic_coord_index]
-            gdf_content += f"{topic}_{subtopic},{subtopic},{individual_radius_subtopics},{sub_x},{sub_y}\n"
-            subtopic_coord_index += 1
+            sub_x, sub_y = subtopic_coords.pop(0)
+            gdf_content += f"{topic}_{subtopic},{subtopic},{individual_radius_subtopics},{sub_x},{sub_y},{color}\n"
 
-    # Edges
     gdf_content += "edgedef>node1 VARCHAR,node2 VARCHAR,directed BOOLEAN\n"
+
+    # Directed edges from each topic to the next and to each of its subtopics
     topics = list(topics_and_subtopics.keys())
     for i, topic in enumerate(topics):
         next_topic = topics[(i + 1) % len(topics)]
         gdf_content += f"{topic},{next_topic},true\n"
+
         for subtopic in topics_and_subtopics[topic]:
             gdf_content += f"{topic},{topic}_{subtopic},true\n"
 
     return gdf_content
 
-
-
-# Example usage with hypothetical data
-# main_topic_coords and subtopic_coords need to be provided with the actual coordinates
+#######################################################
+# Main()
+#######################################################
 
 
 # Parameters for individual topic and subtopic radii
+topics_and_subtopics = mt.topics_and_subtopics
+subsub_topics = mt.subsub_topics
 individual_radius_main_topics = 50
 individual_radius_subtopics = 30
+separation_main_topics = 2.0
+separation_sub_topics = 1.5
+topic_colors = mt.topic_colors
 
 # Generate and round coordinates
-main_topic_coords, subtopic_coords = generate_coordinates(topics_and_subtopics, individual_radius_main_topics, individual_radius_subtopics)
+main_topic_coords, subtopic_coords = generate_coordinates(topics_and_subtopics, individual_radius_main_topics, individual_radius_subtopics,separation_main_topics,separation_sub_topics)
 rounded_main_topic_coords = round_coordinates(main_topic_coords)
 rounded_subtopic_coords = round_coordinates(subtopic_coords)
 
@@ -190,4 +116,4 @@ print("\nRounded Subtopic Coordinates:")
 for coord in rounded_subtopic_coords:
     print(coord)
 
-print(generate_gephi_gdf(topics_and_subtopics,rounded_main_topic_coords,rounded_subtopic_coords,individual_radius_main_topics,individual_radius_subtopics))
+print(generate_gephi_gdf(topics_and_subtopics,rounded_main_topic_coords,rounded_subtopic_coords,individual_radius_main_topics,individual_radius_subtopics,topic_colors))
---
+++ math_taxonomy.py
@@ -0,0 +1,171 @@
+topic_colors = { # Green to Red
+    "Arithmetic": "97,130,100", 
+    "Algebra": "97,130,100",    
+    "Geometry": "121,172,120",
+    "Trigonometry": "176,217,177",
+    "Statistics and Probability": "228,247,143",
+    "Pre-Calculus": "253,255,188",
+    "Calculus": "255,238,187",
+    "Advanced Calculus": "255,220,184",
+    "Discrete Mathematics": "255,193,182",
+    "Linear Algebra": "250,135,127",
+    "Advanced Statistics": "239,75,75",
+    "Mathematical Proofs and Theory": "189,87,78"
+}
+
+topics_and_subtopics = {
+    "Arithmetic": [
+        "Basic operations",
+        "Fractions",
+        "Decimals",
+        "Percentages",
+        "Ratios and Proportions"
+    ],
+    "Algebra": [
+        "Algebraic Expressions",
+        "Linear Equations",
+        "Inequalities",
+        "Systems of Equations",
+        "Quadratic Equations",
+        "Polynomials",
+        "Functions"
+    ],
+    "Geometry": [
+        "Basic Geometric Shapes",
+        "Congruence and Similarity",
+        "Pythagorean Theorem",
+        "Circles",
+        "Area and Volume",
+        "Coordinate Geometry",
+        "Trigonometry"
+    ],
+    "Trigonometry": [
+        "Trigonometric Ratios",
+        "Graphs of Trigonometric Functions",
+        "Trigonometric Identities",
+        "Applications"
+    ],
+    "Statistics and Probability": [
+        "Descriptive Statistics",
+        "Probability Basics",
+        "Combinations and Permutations",
+        "Random Variables and Distributions",
+        "Inferential Statistics"
+    ],
+    "Pre-Calculus": [
+        "Advanced Algebra",
+        "Complex Numbers",
+        "Exponential and Logarithmic Functions",
+        "Advanced Trigonometry",
+        "Sequences and Series",
+        "Matrices"
+    ],
+    "Calculus": [
+        "Limits",
+        "Derivatives",
+        "Integration",
+        "Differential Equations",
+        "Multivariable Calculus"
+    ],
+    "Advanced Calculus": [
+        "Series and Sequences",
+        "Vector Calculus",
+        "Multivariable Calculus",
+        "Differential Equations",
+        "Special Topics"
+    ],
+    "Discrete Mathematics": [
+        "Logic and Proofs",
+        "Set Theory",
+        "Combinatorics",
+        "Graph Theory",
+        "Algorithms and Complexity"
+    ],
+    "Linear Algebra": [
+        "Vector Spaces",
+        "Linear Transformations",
+        "Matrices",
+        "Systems of Linear Equations",
+        "Advanced Topics"
+    ],
+    "Advanced Statistics": [
+        "Regression Analysis",
+        "ANOVA",
+        "Nonparametric Tests",
+        "Time Series Analysis",
+        "Bayesian Statistics"
+    ],
+    "Mathematical Proofs and Theory": [
+        "Introduction to Proofs",
+        "Number Theory",
+        "Group Theory",
+        "Real Analysis",
+        "Topology"
+    ]
+}
+
+
+
+subsub_topics = {
+    "Basic Operations": ["Addition", "Subtraction", "Multiplication", "Division"],
+    "Fractions": ["Understanding", "Simplifying", "Operations"],
+    "Decimals": ["Conversion", "Operations"],
+    "Percentages": ["Calculating percentages", "Percentage increase/decrease", "Real-world applications"],
+    "Ratios and Proportions": ["Understanding ratios", "Solving proportion problems", "Direct and inverse variation"],
+    "Algebraic Expressions": ["Simplifying", "Evaluating expressions", "Combining like terms"],
+    "Linear Equations": ["Solving one-variable", "Multi-variable equations", "Word problems"],
+    "Inequalities": ["Solving", "Graphing on a number line", "Compound inequalities"],
+    "Systems of Equations": ["Methods of solving", "Applications"],
+    "Quadratic Equations": ["Factoring", "Quadratic formula", "Completing the square", "Graphing parabolas"],
+    "Polynomials": ["Adding", "Subtracting", "Multiplying", "Dividing", "Polynomial long division"],
+    "Functions": ["Understanding function notation", "Domain and range", "Inverse functions"],
+    "Basic Geometric Shapes": ["Properties of triangles", "Quadrilaterals", "Polygons"],
+    "Congruence and Similarity": ["Criteria for triangles", "Proving figures congruent or similar"],
+    "Pythagorean Theorem": ["Derivation", "Applications in different contexts"],
+    "Circles": ["Properties", "Arc length", "Sector area", "Theorems involving tangents and chords"],
+    "Area and Volume": ["Formulas for various shapes", "Surface area and volume of 3D figures"],
+    "Coordinate Geometry": ["Equation of a line", "Distance formula", "Midpoint formula"],
+    "Trigonometry": ["Introduction to sine", "Cosine", "Tangent", "Solving right triangles"],
+    "Trigonometric Ratios": ["Definitions", "Solving for unknown sides or angles in right triangles"],
+    "Graphs of Trigonometric Functions": ["Understanding amplitude", "Period", "Phase shift"],
+    "Trigonometric Identities": ["Pythagorean", "Sum and difference", "Double-angle", "Half-angle formulas"],
+    "Applications": ["Real-world problems", "Wave motion", "Circular motion"],
+    "Descriptive Statistics": ["Measures of central tendency", "Measures of dispersion", "Box plots"],
+    "Probability Basics": ["Sample space", "Event probability", "Compound events", "Independent and dependent events"],
+    "Combinations and Permutations": ["Calculating factorial", "Arrangements", "Choosing elements from a set"],
+    "Random Variables and Distributions": ["Understanding discrete and continuous random variables", "Normal distribution"],
+    "Inferential Statistics": ["Basic concepts of hypothesis testing", "Confidence intervals"],
+    "Advanced Algebra": ["Polynomial division", "Synthetic division", "Rational expressions"],
+    "Complex Numbers": ["Operations", "Polar form", "De Moivre's Theorem"],
+    "Exponential and Logarithmic Functions": ["Properties", "Solving exponential and logarithmic equations"],
+    "Advanced Trigonometry": ["Solving general trigonometric equations", "Inverse trigonometric functions"],
+    "Sequences and Series": ["Arithmetic and geometric sequences", "Sums", "Introduction to infinite series"],
+    "Matrices": ["Operations", "Determinants", "Inverse of a matrix", "Applications in solving systems"],
+    "Limits": ["Concept of a limit", "Finding limits graphically and numerically", "Limit laws"],
+    "Derivatives": ["Definition", "Techniques of differentiation", "Applications to motion and optimization"],
+    "Integration": ["Antiderivatives", "Definite integrals", "The Fundamental Theorem of Calculus", "Area under a curve"],
+    "Differential Equations": ["Basic types and solutions", "Applications to growth and decay problems"],
+    "Multivariable Calculus": ["Partial derivatives", "Double and triple integrals"],
+    "Series and Sequences": ["Taylor and Maclaurin series", "Convergence tests", "Applications"],
+    "Vector Calculus": ["Vector functions", "Derivatives and integrals of vector functions", "Line and surface integrals"],
+    "Special Topics": ["Green’s Theorem", "Stokes' Theorem", "Divergence Theorem"],
+    "Logic and Proofs": ["Propositional logic", "Predicate logic", "Methods of proof"],
+    "Set Theory": ["Basic concepts", "Venn diagrams", "Operations on sets"],
+    "Combinatorics": ["Advanced counting techniques", "Binomial theorem", "Generating functions"],
+    "Graph Theory": ["Graphs and their properties", "Eulerian and Hamiltonian paths", "Graph coloring"],
+    "Algorithms and Complexity": ["Algorithm design", "Complexity classes", "Big O notation"],
+    "Vector Spaces": ["Definitions and properties", "Subspaces", "Basis and dimension"],
+    "Linear Transformations": ["Matrix representation", "Geometric transformations"],
+    "Systems of Linear Equations": ["Solution methods", "Applications"],
+    "Advanced Topics": ["Orthogonality", "Least squares", "Singular value decomposition"],
+    "Regression Analysis": ["Simple and multiple linear regression", "Model fitting"],
+    "ANOVA": ["Single-factor and multi-factor ANOVA"],
+    "Nonparametric Tests": ["Chi-square tests", "Mann-Whitney U test", "Kruskal-Wallis test"],
+    "Time Series Analysis": ["Components of time series", "ARIMA models"],
+    "Bayesian Statistics": ["Bayesian inference", "Bayes' theorem"],
+    "Introduction to Proofs": ["Logic and set theory based proofs", "Direct and indirect proofs"],
+    "Number Theory": ["Prime numbers", "Modular arithmetic", "Fermat's Little Theorem"],
+    "Group Theory": ["Basic properties of groups", "Subgroups", "Cyclic groups"],
+    "Real Analysis": ["Sequences and series of real numbers", "Continuity", "Differentiability"],
+    "Topology": ["Basic concepts", "Topological spaces", "Continuity", "Compactness"]
+}
