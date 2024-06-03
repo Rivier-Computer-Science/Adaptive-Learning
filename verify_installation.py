5ab5ff872fb4ad65e4612b0bd6199c6ca0d2e9a4
Anudeep Gumpula <anudeepgumpula@gmail.com>
2024-06-02 20:34:51 -0400

    Update verify_installation.py

--- verify_installation.py
+++ verify_installation.py
@@ -1,25 +1,6 @@
-from transformers import AutoModelForCausalLM, AutoTokenizer
 
-<<<<<<< HEAD
-def verify_installation():
-    print("Starting verification...")
-    
-    # Load tokenizer
-    tokenizer_name = "EleutherAI/llemma_7b"
-    tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
-    print("Tokenizer loaded successfully.")
-    
-    # Load model
-    print("Attempting to load the model...")
-    model = AutoModelForCausalLM.from_pretrained(tokenizer_name)
-    print("Model loaded successfully.")
-    
-    print("Verification completed successfully.")
-=======
 model_name = "EleutherAI/llemma_7b"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
->>>>>>> origin/SU24-Sprint-1
 
-if __name__ == "__main__":
-    verify_installation()
+print("Model and tokenizer loaded successfully.")
