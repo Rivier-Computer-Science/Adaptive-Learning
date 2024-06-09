27f116d6f391b9ce4898c812101c2d8ed84ad9c6
Anudeep Gumpula <anudeepgumpula@gmail.com>
2024-06-08 22:22:09 -0400

    1st description commit

--- src/Agents/knowledge_tracer_agent.py
+++ src/Agents/knowledge_tracer_agent.py
@@ -4,9 +4,6 @@ from src.Models.llm_config import gpt3_config
 
 class KnowledgeTracerAgent(MyConversableAgent):
     description =   """You are a Knowledge Tracer.
-                         You test the student on what they know.
-                         You work with the Problem Generator to present problems to the Student.
-                         You work with the Learner Model to keep track of the Student's level.
                     """
     def __init__(self):
         super().__init__(
