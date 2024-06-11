562750fbd66e81d6c190bf68342fd54690293278
Anudeep Gumpula <anudeepgumpula@gmail.com>
2024-06-10 23:52:15 -0400

    fifth description commit

--- src/Agents/knowledge_tracer_agent.py
+++ src/Agents/knowledge_tracer_agent.py
@@ -3,7 +3,10 @@ from .conversable_agent import MyConversableAgent
 from src.Models.llm_config import gpt3_config
 
 class KnowledgeTracerAgent(MyConversableAgent):
-    description =   """You work with the Learner Model to keep track of the Student's level."""
+    description =   """You are a Knowledge Tracer.
+                         You test the student on what they know.
+                         You work with the Problem Generator to present problems to the Student.
+                         You work with the Learner Model to keep track of the Student's level."""
     def __init__(self):
         super().__init__(
                 name="KnowledgeTracer",
