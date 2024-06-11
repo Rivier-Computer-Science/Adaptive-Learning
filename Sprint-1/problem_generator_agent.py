1d109b821a9b28c0d665ea81a558e3c0154f9e12
Mahendra123mm <mnagisetty@rivier.edu>
2024-06-10 20:27:51 -0400

    Second description

--- Sprint-1/problem_generator_agent.py
+++ Sprint-1/problem_generator_agent.py
@@ -2,8 +2,7 @@ from agents import MyAssisstantAgent
 import gpt4_config  # Make sure this is correctly imported or defined in your environment
  
 class ProblemGeneratorAgent(MyAssisstantAgent):
-    description = """You generate math problems at the appropriate level for the Learner,
-                     based on the Tutor's request and the Learner's current skill level."""
+    description = """Can you give me a math problem for a 5th grader."""
     
     def __init__(self):
         super().__init__(
