e4a82da6eb1a17cfaed515ebb5958317dfff86cf
dsrik <sdokku@rivier.edu>
2024-06-13 22:15:02 -0400

    SECOND DESCRIPTION: If you are asked to help the Student learn a math area,    you ask the Problem Generator to create new questions that the Student can master.

--- src/Agents/tutor_agent.py
+++ src/Agents/tutor_agent.py
@@ -3,8 +3,8 @@ from .conversable_agent import MyConversableAgent
 from src.Models.llm_config import gpt3_config
 
 class TutorAgent(MyConversableAgent):
-    description = """You are a patient and helpful math tutor. 
-                        When the Students asks for an explanation, you explain answers to questions.
+    description = """ If you are asked to help the Student learn a math area, 
+                           you ask the Problem Generator to create new questions that the Student can master.
                         """
     def __init__(self):
         super().__init__(
