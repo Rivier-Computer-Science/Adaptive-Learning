2aeb139177d8382e692c99ff1f0a7f4e4a7967f7
Anudeep Gumpula <anudeepgumpula@gmail.com>
2024-06-16 21:05:32 -0400

    Spike-1.02b Description-" I "

--- src/Agents/knowledge_tracer_agent.py
+++ src/Agents/knowledge_tracer_agent.py
@@ -3,26 +3,29 @@ from .conversable_agent import MyConversableAgent
 from src.Models.llm_config import gpt3_config
 
 class KnowledgeTracerAgent(MyConversableAgent):
-    description_you = """You are a Knowledge Tracer.
-                         You test the student on what they know.
-                         You work with the Problem Generator to present problems to the Student.
-                         You work with the Learner Model to keep track of the Student's level.
+    description_i = """I am a Knowledge Tracer.
+                       My role is to test the student on what they know,
+                       present problems through the Problem Generator,
+                       and maintain the Learner Model to track the student's level.
+                    """
+    system_message_i = """As a Knowledge Tracer, my role is to test your knowledge,
+                         present problems through the Problem Generator,
+                         and maintain the Learner Model to track your level.
                       """
-    system_message_you = """As a Knowledge Tracer, I will help you test your knowledge,
-                            present problems through the Problem Generator,
-                            and track your progress with the Learner Model.
-                         """
     
     def __init__(self):
         super().__init__(
             name="KnowledgeTracer",
             human_input_mode="NEVER",
             llm_config=gpt3_config,
-            system_message=self.system_message_you,
-            description=self.description_you
+            system_message=self.system_message_i,
+            description=self.description_i
         )
         self.last_plot_request = None  # Add an attribute to track plot requests
         self.groupchat = None
+
+ 
+
 
 
 
