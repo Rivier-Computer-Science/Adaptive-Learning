2557680438f17cd5a6bbcd9cde74585c6c478f18
Anudeep Gumpula <anudeepgumpula@gmail.com>
2024-06-16 22:06:21 -0400

    Spike-1.02b Description-" Agent name - Knowledge Tracer "

--- src/Agents/knowledge_tracer_agent.py
+++ src/Agents/knowledge_tracer_agent.py
@@ -1,30 +1,27 @@
-##################### Knowledge Tracer ##########################
+##################### Knowledge Tracer #########################
 from .conversable_agent import MyConversableAgent
 from src.Models.llm_config import gpt3_config
 
 class KnowledgeTracerAgent(MyConversableAgent):
-    description_i = """Knowledge Tracer is an agent tasked with testing the student's knowledge,
+    description_agent = """Knowledge Tracer is an agent tasked with testing the student's knowledge,
                            presenting problems with the Problem Generator,
                            and maintaining the Learner Model to track the student's level.
-                    """
-    system_message_i = """As a Knowledge Tracer, my role is to test your knowledge,
-                         present problems through the Problem Generator,
-                         and maintain the Learner Model to track your level.
-                      """
+                        """
+    system_message_agent = """Knowledge Tracer's function is to test the student's knowledge,
+                             present problems through the Problem Generator,
+                             and maintain the Learner Model to track the student's level.
+                          """
     
     def __init__(self):
         super().__init__(
             name="KnowledgeTracer",
             human_input_mode="NEVER",
             llm_config=gpt3_config,
-            system_message=self.system_message_i,
-            description=self.description_i
+            system_message=self.system_message_agent,
+            description=self.description_agent
         )
         self.last_plot_request = None  # Add an attribute to track plot requests
         self.groupchat = None
 
  
-
-
-
 
