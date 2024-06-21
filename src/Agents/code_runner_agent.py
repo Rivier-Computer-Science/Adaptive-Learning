217d1e187a877c10ae335f396fdafbf2c1b1e5d5
kavitha <kavithasrikoya@gmail.com>
2024-06-20 23:45:51 -0400

    Add Code Runner spike 1b.08 Code Runner Description

--- src/Agents/code_runner_agent.py
+++ src/Agents/code_runner_agent.py
@@ -3,7 +3,7 @@ from .conversable_agent import MyConversableAgent
 from src.Models.llm_config import gpt3_config
 
 class CodeRunnerAgent(MyConversableAgent):  
-    description = "As a vital component of a collaborative agent framework, I specialize in executing and displaying code outputs. I interact seamlessly with educational and development agents, enhancing learning and programming experiences. By providing real-time feedback on code execution, I support users and other agents in refining and understanding complex code segments, contributing to a more robust and interactive learning environment."
+    description = """As a vital component of a collaborative agent framework, Code Runner specializes in executing and displaying code outputs. Code Runner interacts seamlessly with educational and development agents, enhancing learning and programming experiences. By providing real-time feedback on code execution, Code Runner support users and other agents in refining and understanding complex code segments, contributing to a more robust and interactive learning environment."""
     message = """Code Runner's function is to execute and display code outputs, providing real-time feedback. Code Runner interacts seamlessly with educational and development agents, enhancing learning and programming experiences. By refining and understanding complex code segments, Code Runner supports users and other agents, contributing to a more robust and interactive learning environment. """
     def __init__(self):
         super().__init__(
