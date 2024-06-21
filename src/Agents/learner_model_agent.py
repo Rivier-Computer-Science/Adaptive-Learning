618d30d00dca3175b9fd95e1b488fa247b945724
Baswanth <kbaswanthchowdary@gmail.com>
2024-06-20 23:25:04 -0400

    Spike 1b.09 Learner Model description

--- src/Agents/learner_model_agent.py
+++ src/Agents/learner_model_agent.py
@@ -3,7 +3,7 @@ from .conversable_agent import MyConversableAgent
 from src.Models.llm_config import gpt3_config
 
 class LearnerModelAgent(MyConversableAgent):
-    description = """I am a diligent and meticulous learning tracker. I assess the Student's educational journey, adapting learning paths by collaborating with the Tutor and Knowledge Tracer. I analyze performance data to provide feedback, help set educational goals, and adjust the difficulty of tasks. I ensure that the learning experience is tailored to the Student’s evolving capabilities and needs."""
+    description = """Learner Model is a diligent and meticulous learning tracker. Learner Model assess the Student's educational journey, adapting learning paths by collaborating with the Tutor and Knowledge Tracer. Learner Model analyzes performance data to provide feedback, help set educational goals, and adjust the difficulty of tasks. Learner Model ensures that the learning experience is tailored to the Student’s evolving capabilities and needs."""
     message = """Learner Model's function is to diligently track the Student's educational journey. Learner Model assesses performance data, collaborates with the Tutor and Knowledge Tracer, and adapts learning paths to provide feedback. Learner Model helps set educational goals and adjusts the difficulty of tasks, ensuring that the learning experience is tailored to the Student’s evolving capabilities and needs. """
     def __init__(self):
         super().__init__(
