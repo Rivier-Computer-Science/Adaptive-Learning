f4a96ad127b6c9a7f70a3e5b90c3a721b711c86b
Anudeep Gumpula <anudeepgumpula@gmail.com>
2024-07-09 19:16:17 -0400

    updated gpt 4

--- src/UI/panel_integrated_agents.py
+++ src/UI/panel_integrated_agents.py
@@ -6,13 +6,14 @@ import time
 import asyncio
 from typing import List, Dict
 import logging
+import json
 from src import globals
-from src.UI.avatar import avatar
 
-os.environ["AUTOGEN_USE_DOCKER"] = "False"
-
-globals.input_future = None
-
+############################################################################################
+#
+#  DEFINE AGENTS
+#
+############################################################################################
 from ..Agents.base_agent import MyBaseAgent
 from ..Agents.conversable_agent import MyConversableAgent
 from ..Agents.student_agent import StudentAgent
@@ -27,9 +28,40 @@ from ..Agents.learner_model_agent import LearnerModelAgent
 from ..Agents.level_adapter_agent import LevelAdapterAgent
 from ..Agents.motivator_agent import MotivatorAgent
 from ..Agents.group_chat_manager_agent import CustomGroupChatManager
+#from src.Agents.agents import *
+from src.UI.avatar import avatar
+#from src.Agents.group_chat_manager_agent import CustomGroupChatManager
+import pprint
 
+script_dir = os.path.dirname(os.path.abspath(__file__))
+progress_file_path = os.path.join(script_dir, '../../progress.json')
+
+os.environ["AUTOGEN_USE_DOCKER"] = "0"
+
+globals.input_future = None
+
+# Agents
+#####################################################################################
+# I've set all the Agents to the prompts found to be "best" in Sprint-2
+# 
+# Below are examplex of how to override defaults in this file instead of agent file
+#
+# I believe these are the only 3 you will need to change
+#
+# If you find otherwise, you will need to directly update the agents
+#    becasue I only created constructors for these additional parameters
+#
+# See the README.md file for description vs system_message
+##########################################################################
+
+#######################################
+# Student was not completed in Sprint-2
+#######################################
 student = StudentAgent()
 
+###################
+# Knowledge Tracer
+##################
 kt_description = """You are a Knowledge Tracer.
                     You test the student on what they know.
                     You work with the Problem Generator to present problems to the Student.
@@ -41,6 +73,9 @@ knowledge_tracer = KnowledgeTracerAgent(
     system_message=kt_description    
 )
 
+###################
+# Teacher
+###################
 t_description =   """You are a Teacher.
                  When asked by the Student to learn new material, you present clear and concise lecture-type material.
                  """
@@ -50,6 +85,9 @@ teacher = TeacherAgent(
     system_message=t_description     
 )
 
+###################
+# Tutor
+###################
 tut_description = """  TutorAgent is designed to assist students in real-time with their math problems. It offers solutions and explanations, responding effectively to inquiries to support adaptive learning. TutorAgent's goal is to make learning easier and more interactive for students.
                         """
 tutor = TutorAgent(
@@ -58,6 +96,9 @@ tutor = TutorAgent(
     system_message=tut_description      
 )
 
+###################
+# Problem Generator
+###################
 pg_description = """ProblemGenerator is designed to generate mathematical problems based on the current curriculum and the student's learning level.
                 ProblemGenerator ensures that the problems generated are appropriate and challenging."""
                     
@@ -70,6 +111,9 @@ problem_generator = ProblemGeneratorAgent(
     system_message=pg_system_message     
 )
 
+###################
+# Solution Verifier
+###################
 sv_description = """SolutionVerifierAgent ensures the accuracy of solutions provided for various problems. SolutionVerifierAgent checks solutions against the correct answers and offers feedback on their correctness."""
     
 sv_system_message = """SolutionVerifierAgent's task is to verify the correctness of solutions submitted by comparing them against the correct answers and providing feedback on their accuracy."""
@@ -80,8 +124,14 @@ solution_verifier = SolutionVerifierAgent(
     system_message=sv_description     
 )
 
+###########################################
+# Programmer was not completed in Sprint-2
+###########################################
 programmer = ProgrammerAgent()
 
+###################
+# Code Runner
+###################
 cr_description = """As a vital component of a collaborative agent framework, Code Runner specializes in executing and displaying code outputs. Code Runner interacts seamlessly with educational and development agents, enhancing learning and programming experiences. By providing real-time feedback on code execution, Code Runner support users and other agents in refining and understanding complex code segments, contributing to a more robust and interactive learning environment."""
 cr_system_message = """Code Runner's function is to execute and display code outputs, providing real-time feedback. Code Runner interacts seamlessly with educational and development agents, enhancing learning and programming experiences. By refining and understanding complex code segments, Code Runner supports users and other agents, contributing to a more robust and interactive learning environment. """
 code_runner = CodeRunnerAgent(
@@ -90,6 +140,9 @@ code_runner = CodeRunnerAgent(
     system_message=cr_system_message  
 )
 
+###################
+# Level Adapter
+###################
 lv_description ="""
     LevelAdapter is an agent that interacts with the Learner Model to fetch information about the Student's learning progress.
     LevelAdapter provides input to other agents or systems based on the Student's level.
@@ -104,6 +157,9 @@ level_adapter = LevelAdapterAgent(
     system_message=lv_system_message   
 )
 
+###################
+# Motivator
+###################
 m_description = """ You provide positive and encouraging feedback to the Student to keep them motivated.
                         Only provide motivation to the Student. 
                         Offer specific praise and acknowledge the Student's effort and progress.
@@ -115,6 +171,9 @@ motivator = MotivatorAgent(
     system_message=m_description  
 )
 
+###################
+# Level Model
+###################
 lm_description="""Learner Model is a diligent and meticulous learning tracker. Learner Model assess the Student's educational journey, adapting learning paths by collaborating with the Tutor and Knowledge Tracer. Learner Model analyzes performance data to provide feedback, help set educational goals, and adjust the difficulty of tasks. Learner Model ensures that the learning experience is tailored to the Student’s evolving capabilities and needs."""
 lm_system_message="""Learner Model's function is to diligently track the Student's educational journey. Learner Model assesses performance data, collaborates with the Tutor and Knowledge Tracer, and adapts learning paths to provide feedback. Learner Model helps set educational goals and adjusts the difficulty of tasks, ensuring that the learning experience is tailored to the Student’s evolving capabilities and needs. """        
 learner_model = LearnerModelAgent(
@@ -122,6 +181,8 @@ learner_model = LearnerModelAgent(
     description=lm_description,
     system_message=lm_system_message
 )
+
+
 
 agents_dict = {
     "student": student,
@@ -137,51 +198,147 @@ agents_dict = {
     "motivator": motivator
 }
 
-TRANSITIONS = 'DISALLOWED'
 
-disallowed_agent_transitions = {
-    student: [solution_verifier, programmer, code_runner, learner_model, level_adapter, motivator],
-    tutor: [programmer, code_runner],
-    teacher: [knowledge_tracer, problem_generator, solution_verifier, programmer, code_runner, learner_model, level_adapter, motivator],
-    knowledge_tracer: [teacher, tutor, motivator],
-    problem_generator: [teacher, solution_verifier, programmer, code_runner, motivator],
-    solution_verifier: [student, teacher, problem_generator, learner_model, level_adapter, motivator],
-    programmer: [student, tutor, teacher, knowledge_tracer, learner_model, level_adapter, motivator],
-    code_runner: [student, teacher, tutor, knowledge_tracer, problem_generator, learner_model, level_adapter, motivator],
-    learner_model: [student, teacher, problem_generator, solution_verifier, programmer, code_runner],
-    level_adapter: [student, teacher, solution_verifier, programmer, code_runner, motivator],
-    motivator: [teacher, problem_generator, solution_verifier, programmer, code_runner, learner_model]
-}
 
-chat_manager = CustomGroupChatManager(agents_dict, disallowed_agent_transitions)
+agents = list(agents_dict.values())
+#agents = [student, tutor]
+
+groupchat = autogen.GroupChat(agents=agents,
+                              messages=[],
+                              max_round=40,
+                              send_introductions=True,
+                              )
+
+manager = CustomGroupChatManager(termination_string='exit', filename=progress_file_path, groupchat=groupchat )    
+
+####################################################################################################
+#
+#  Define Agent Transitions: Unconstrained, Allowed, or Disallowed
+#
+####################################################################################################
+TRANSITIONS = 'UNCONSTRAINED'      # Set TRANSITIONS for type
+if TRANSITIONS == 'DISALLOWED':
+
+    disallowed_agent_transitions = {
+        student: [solution_verifier, programmer, code_runner, learner_model, level_adapter, motivator],
+        tutor: [programmer, code_runner],
+        teacher: [knowledge_tracer, problem_generator, solution_verifier, programmer, code_runner, learner_model, level_adapter, motivator],
+        knowledge_tracer: [teacher, tutor, motivator],
+        problem_generator: [teacher, solution_verifier, programmer, code_runner, motivator],
+        solution_verifier: [student, teacher, problem_generator, learner_model, level_adapter, motivator],
+        programmer: [student, tutor, teacher, knowledge_tracer, learner_model, level_adapter, motivator],
+        code_runner: [student, teacher, tutor, knowledge_tracer, problem_generator, learner_model, level_adapter, motivator],
+        learner_model: [student, teacher, problem_generator, solution_verifier, programmer, code_runner],
+        level_adapter: [student, teacher, solution_verifier, programmer, code_runner, motivator],
+        motivator: [tutor, teacher, knowledge_tracer, problem_generator, solution_verifier, programmer, code_runner, learner_model, level_adapter]
+    }
+    groupchat = autogen.GroupChat(agents=list(agents_dict.values()), 
+                                messages=[],
+                                max_round=40,
+                                send_introductions=True,
+                                speaker_transitions_type="disallowed",
+                                allowed_or_disallowed_speaker_transitions=disallowed_agent_transitions,
+                                )
+    
+elif TRANSITIONS == 'ALLOWED':
+    allowed_agent_transitions = {
+        student: [tutor],
+        tutor: [student, teacher, problem_generator, solution_verifier, motivator],
+        teacher: [student, tutor, learner_model],
+        knowledge_tracer: [student, problem_generator, learner_model, level_adapter],
+        problem_generator: [tutor],
+        solution_verifier: [programmer],
+        programmer: [code_runner],
+        code_runner: [tutor, solution_verifier],
+        learner_model: [knowledge_tracer, level_adapter],
+        level_adapter: [tutor, problem_generator, learner_model],
+        motivator: [tutor]
+    }
+    groupchat = autogen.GroupChat(agents=list(agents_dict.values()), 
+                              messages=[],
+                              max_round=40,
+                              send_introductions=True,
+                              speaker_transitions_type="allowed",
+                              allowed_or_disallowed_speaker_transitions=allowed_agent_transitions,
+                               )
+
+else:  # Unconstrained
+    agents = list(agents_dict.values()) # All agents
+    groupchat = autogen.GroupChat(agents=agents, 
+                              messages=[],
+                              max_round=40,
+                              send_introductions=True,
+                              )
+
+
+
+
+
+manager = CustomGroupChatManager(groupchat=groupchat)
 
 def create_app():
-    output_pane = pn.pane.Markdown("", sizing_mode="stretch_both")
-    chat_input = pn.widgets.TextInput(placeholder="Type your message here...", sizing_mode="stretch_width")
-    send_button = pn.widgets.Button(name="Send", button_type="primary", sizing_mode="fixed")
+    pn.extension(design="material")
 
-    async def process_input(event):
-        user_input = chat_input.value
-        chat_input.value = ""
+    async def callback(contents: str, user: str, instance: pn.chat.ChatInterface):
+        if not globals.initiate_chat_task_created:
+            asyncio.create_task(manager.delayed_initiate_chat(tutor, manager, contents))
+        else:
+            if globals.input_future and not globals.input_future.done():
+                globals.input_future.set_result(contents)
+            else:
+                print("No input being awaited.")
 
-        student_response = await chat_manager.chat(student, user_input)
-        tutor_response = await chat_manager.chat(tutor, student_response.content)
+    chat_interface = pn.chat.ChatInterface(callback=callback)
 
-        output_pane.object += f"**Student:** {user_input}\n\n"
-        output_pane.object += f"**Tutor:** {tutor_response.content}\n\n"
+    def print_messages(recipient, messages, sender, config):
+        print(f"Messages from: {sender.name} sent to: {recipient.name} | num messages: {len(messages)} | message: {messages[-1]}")
 
-    send_button.on_click(lambda event: asyncio.create_task(process_input(event)))
+        content = messages[-1]['content']
 
-    app_layout = pn.Column(
-        avatar,
-        output_pane,
-        pn.Row(chat_input, send_button),
-        sizing_mode="stretch_both"
+        # Capture and store messages in _chat_messages
+        recipient._chat_messages.setdefault(recipient, []).append({
+            'content': content,
+            'name': messages[-1].get('name', sender.name),
+            'role': messages[-1].get('role', 'user')
+        })
+
+        if all(key in messages[-1] for key in ['name']):
+            chat_interface.send(content, user=messages[-1]['name'], avatar=avatar[messages[-1]['name']], respond=False)
+        else:
+            chat_interface.send(content, user=recipient.name, avatar=avatar[recipient.name], respond=False)
+
+        return False, None
+
+    for agent in groupchat.agents:
+        agent.chat_interface = chat_interface
+        agent.register_reply([autogen.Agent, None], reply_func=print_messages, config={"callback": None})
+
+    app = pn.template.BootstrapTemplate(title=globals.APP_NAME)
+    app.main.append(
+        pn.Column(
+            chat_interface
+        )
     )
+  
 
-    return app_layout
+    #Load chat history on startup 
+    chat_history_messages = manager.get_messages_from_json()
+    if chat_history_messages:
+        manager.resume(chat_history_messages, 'exit')
+        for message in chat_history_messages:
+            if 'exit' not in message:
+                chat_interface.send(
+                    message["content"],
+                    user=message["role"], 
+                    avatar=avatar.get(message["role"], None),  
+                    respond=False
+                )
+        chat_interface.send("Time to continue your studies!", user="System", respond=False)
+    else:
+        chat_interface.send("Welcome to the Adaptive Math Tutor! How can I help you today?", user="System", respond=False)
 
-if __name__.startswith("bokeh"):
-    pn.extension()
+    return app
+
+if __name__ == "__main__":
     app = create_app()
-    app.show(port=5007)
+    pn.serve(app)
