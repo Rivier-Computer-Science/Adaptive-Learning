f69a577c2ad6f450adc8b29a1326d70ca8797b9b
glossner <john@glossner.org>
2024-07-14 12:59:47 -0400

    Update model to GPT-4o

--- src/Agents/conversable_agent.py
+++ src/Agents/conversable_agent.py
@@ -8,7 +8,7 @@ from src.Models.llm_config import gpt3_config, gpt4_config
 
 from .base_agent import MyBaseAgent
 
-llm = gpt3_config
+llm = gpt4_config
 
 class MyConversableAgent(autogen.ConversableAgent, MyBaseAgent):
     def __init__(self, **kwargs):
