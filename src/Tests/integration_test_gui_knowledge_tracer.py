7017a11786fd436136902e61081637ddcc6b111e
Praveen Rayankula <Prayankula@rivier.edu>
2024-10-15 21:28:53 -0600

    Add integration test functions

--- src/Tests/integration_test_gui_knowledge_tracer.py
+++ src/Tests/integration_test_gui_knowledge_tracer.py
@@ -29,5 +29,21 @@ class TestGUIKnowledgeTracerIntegration(unittest.TestCase):
         self.manager = CustomGroupChatManager(groupchat=self.mock_groupchat, filename="test_progress.json")
         self.fsm.register_groupchat_manager(self.manager)
 
+    @patch('src.UI.gui_knowledge_tracer.pn.serve')
+    def test_gui_initialization(self, mock_serve):
+        from src.UI.gui_knowledge_tracer import create_app
+        app = create_app()
+        self.assertIsInstance(app, pn.viewable.Viewable)
+        mock_serve.assert_called_with(app, callback_exception='verbose')
+
+    def test_groupchat_manager_initialization(self):
+        self.manager.get_chat_history_and_initialize_chat(filename="test_progress.json", chat_interface=Mock())
+        self.mock_groupchat.get_messages.assert_called_once()
+
+    def test_integration_with_agents(self):
+        # Ensure that agents' methods like groupchat_manager assignment are being called
+        for agent in self.agents_dict.values():
+            self.assertEqual(agent.groupchat_manager, self.manager)
+
 if __name__ == '__main__':
     unittest.main()
