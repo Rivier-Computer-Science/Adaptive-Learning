374e8b00371724a1718c151fe92edae5bec6ca98
Shivani Sanem <ssanem@rivier.edu>
2024-10-29 13:53:18 -0600

    Updated telugu agent related files for better functionality

--- src/Agents/group_chat_manager_agent.py
+++ src/Agents/group_chat_manager_agent.py
@@ -6,7 +6,8 @@ from typing import Optional, List, Dict
 import panel as pn
 from src import globals
 from src.Agents.agents import agents_dict_by_name
-from src.UI.avatar import avatar
+import src.UI.avatar as avatar
+import logging
 
 
 class CustomGroupChat(autogen.GroupChat):
@@ -41,7 +42,8 @@ class CustomGroupChatManager(autogen.GroupChatManager):
             await super().a_run_chat(**kwargs)
             self.save_messages_to_json(self.filename)
         except Exception as e:
-            print(f"Exception occurred: {e}") 
+            print(f"Exception occurred: {e}")
+            raise 
 
         return True, None
             
@@ -55,7 +57,8 @@ class CustomGroupChatManager(autogen.GroupChatManager):
                 self.messages_from_json = self.messages_from_string(f.read())
                 # Strip termination message and restore chat history
                 if self.messages_from_json:
-                    if self.messages_from_json[-1].get("content","").strip()==globals.IS_TERMINATION_MSG:
+                    #if self.messages_from_json[-1].get("content","").strip()==globals.IS_TERMINATION_MSG:
+                    if self.messages_from_json[-1].get("content","")==globals.IS_TERMINATION_MSG:
                         self.messages_from_json.pop()
                     # Resume the chat from where it leaft off
                     # FIXME: Resume is not working correctly.
@@ -91,7 +94,22 @@ class CustomGroupChatManager(autogen.GroupChatManager):
 
 
     # TODO: Consider moving the writes to the chat panel to reactive_chat
-    def get_chat_history_and_initialize_chat(self, filename: str = None, chat_interface: pn.chat.ChatInterface = None):
+    def get_chat_history_and_initialize_chat(self, 
+                                             initial_message: str = None,
+                                             avatars=None,
+                                             filename: str = None, 
+                                             chat_interface: pn.chat.ChatInterface = None):
+        if initial_message is None:
+            self.initial_message = "Welcome to the Adaptive Math Tutor! How can I help you today?"
+        else:
+            self.initial_message = initial_message
+
+        if avatars is None:
+            self.avatars = avatar.avatar
+        else:
+            self.avatars = avatars
+
+
         chat_history_messages = self.get_messages_from_json(filename=filename)
         # Send the chat history to the panel interface
         if chat_history_messages:        
@@ -100,20 +118,29 @@ class CustomGroupChatManager(autogen.GroupChatManager):
                     chat_interface.send(
                         message["content"],
                         user=message["role"], 
-                        avatar=avatar.get(message["role"], None),  
+                        avatar=self.avatars.get(message["role"], None),  
                         respond=False
                     )
             chat_interface.send("Time to continue your studies!", user="System", respond=False)
         else:
-            chat_interface.send("Welcome to the Adaptive Math Tutor! How can I help you today?", user="System", respond=False)
+            chat_interface.send(self.initial_message, user="System", respond=False)
 
  
     async def delayed_initiate_chat(self, agent, recipient, message):
+        logging.debug("CustomGroupChatManager: delayed_initiate_chat started")
         globals.initiate_chat_task_created = True
-        await asyncio.sleep(1) 
-        await agent.a_initiate_chat(recipient=recipient, 
+        #await asyncio.sleep(1)
+        try:
+            logging.debug(f"agent={agent.name}, recipient={recipient}, message={message}") 
+            chat_result = await agent.a_initiate_chat(recipient=recipient, 
                                     clear_history = False,
                                     message=message)
+            logging.debug(f"chat_result= {chat_result}" )
+        except Exception as e:
+            logging.error(f"Exception occured while calling agent.a_initiate_chat. agent={agent}")
+            raise
+
+        logging.info(f"CustomGroupChatManager: agent.a_initiate_chat() with name {agent.name} and receiptient {recipient} completed")
 
     @property
     def chat_interface(self) ->  pn.chat.ChatInterface:
@@ -122,5 +149,3 @@ class CustomGroupChatManager(autogen.GroupChatManager):
     @chat_interface.setter
     def chat_interface(self, chat_interface: pn.chat.ChatInterface):
         self._chat_interface = chat_interface
-
-
