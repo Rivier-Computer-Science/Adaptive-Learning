48f0a4428f3e12d3ab76e952d7da16fbdcc5aab5
Priya Dondapati <dondapatiL.pegasian@gmail.com>
2024-10-29 14:47:56 -0600

    Update reactive chat and panel gui tabs

---
+++ src/UI/panel_gui_tabs19_priya.py
@@ -0,0 +1,63 @@
+
+
+import autogen
+import panel as pn
+import openai
+import os
+import asyncio
+from typing import List, Dict
+from src import globals
+from src.Agents.agents import *
+from src.Agents.chat_manager_fsms import FSM
+from src.Agents.group_chat_manager_agent import CustomGroupChatManager, CustomGroupChat
+from src.UI.reactive_chat_19 import ReactiveChat
+from src.UI.avatar import avatar
+import logging
+
+logging.basicConfig(level=logging.INFO,
+                    format='%(levelname)s - %(module)s - %(filename)s - %(funcName)s - line %(lineno)d - %(asctime)s - %(name)s - %(message)s')
+
+
+os.environ["AUTOGEN_USE_DOCKER"] = "False"
+
+
+##############################################
+# Main Adaptive Learning Application
+##############################################
+globals.input_future = None
+script_dir = os.path.dirname(os.path.abspath(__file__))
+progress_file_path = os.path.join(script_dir, '../../progress.json')
+
+fsm = FSM(agents_dict)
+
+groupchat = CustomGroupChat(agents=list(agents_dict.values()), 
+                              messages=[],
+                              max_round=globals.MAX_ROUNDS,
+                              send_introductions=True,
+                              speaker_selection_method=fsm.next_speaker_selector
+                              )
+
+manager = CustomGroupChatManager(groupchat=groupchat,
+                                filename=progress_file_path, 
+                                is_termination_msg=lambda x: x.get("content", "").rstrip().find("TERMINATE") >= 0 )    
+
+# Begin GUI components
+reactive_chat = ReactiveChat(groupchat_manager=manager)
+
+# Register groupchat_manager and reactive_chat GUI interface with ConversableAgents
+for agent in groupchat.agents:
+    agent.groupchat_manager = manager
+    agent.reactive_chat = reactive_chat
+    agent.register_reply([autogen.Agent, None], reply_func=agent.autogen_reply_func, config={"callback": None})
+
+reactive_chat.update_dashboard()    # Call after history loaded
+
+# Create app with speech recognition button
+def create_app():    
+    return pn.Column(
+        reactive_chat.draw_view()
+    )
+
+if __name__ == "__main__":    
+    app = create_app()
+    pn.serve(app, callback_exception='verbose')
