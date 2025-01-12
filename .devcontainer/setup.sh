4ef59d14a4cabfbe0c592eb53718fd3f527731b7
glossner <john@glossner.org>
2025-01-12 08:59:21 -0500

    separate cuda and cpu conda environments for codespaces

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -2,9 +2,16 @@
 
 echo "Creating adaptive conda environment. This will take some time (minutes)"
 
+# Ensure Conda is initialized for the current shell
+eval "$(conda shell.bash hook)"
+
 # Create the Conda environment
 conda env create -f conda_env.yml -n adaptive 
+
+# Clean up unnecessary files
 conda clean -a --yes
+
+# Activate the environment
 conda activate adaptive    
 
 echo "Setup completed successfully!"
