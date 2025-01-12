8cf2affd77ead585ecbf227658afc2354defaf01
glossner <john@glossner.org>
2025-01-12 08:20:19 -0500

    remove cuda from yml file for codespaces build

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "Creating adaptive conda environment. This will take some time (minutes)"
+
 # Create the Conda environment
 conda env create -f conda_env.yml -n adaptive 
 conda clean -a 
