5786b915f0841dd73b30d2a214ea4dbc818edc70
GIRIBABU VEMURI <gvemuri@rivier.edu>
2025-06-03 08:01:01 +0530

    Added crewAI module

---
+++ src/crewAI/utils.py
@@ -0,0 +1,13 @@
+# ANSI escape codes for colors
+class Colors:
+    HEADER = '\033[95m'  # Magenta
+    BLUE = '\033[94m'
+    CYAN = '\033[96m'
+    GREEN = '\033[92m'
+    WARNING = '\033[93m' # Yellow
+    FAIL = '\033[91m'    # Red
+    ENDC = '\033[0m'     # Resets color
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    INFO = '\033[94m' # Blue for general info
+    DEBUG = '\033[90m' # Grey for debug
